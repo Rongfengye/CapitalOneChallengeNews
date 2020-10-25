0dea9e96de2689fc5f5717999e600f3c5b0ecfc7
Rong Feng Ye <rongfengye@Rongs-MBP.fios-router.home>
2020-10-25 15:00:06 -0400

    conditionally rendered banners

--- main.js
+++ main.js
@@ -6,19 +6,13 @@ const topicImage = document.querySelector(".topic-image");
 //searchFrom.addEventListener("submit", retrieve);
 
 const apiData = {
-  url: "https://newsapi.org/v2/top-headlines?",
+  url: "http://newsapi.org/v2/top-headlines?",
   lan: "country=us&language=en&",
   cate: "",
   apiK: "apiKey=3680b889e3fe4c128f994a74da6f98db",
-}
+};
 
-// const banners = {
-//   sportB: "pictures/sport-banner.jpg",
-//   eB: "pictures/entertainment-banner.jpg",
-//   techB: "pictures/tech-banner.jpg"
-// }
-
-const proxyUrl = "http://cors-anywhere.herokuapp.com/"
+const proxyUrl = "http://cors-anywhere.herokuapp.com/";
 
 function buttonHandler(topic) {
   const sportKey = "category=sports&";
@@ -26,40 +20,45 @@ function buttonHandler(topic) {
   const techKey = "category=technology&";
   if (topic === "sports") {
     apiData.cate = sportKey;
+    document.getElementById("topic-image").innerHTML =
+      "<img src='pictures/sport-banner.jpg' alt='sportpic' width='100%' />";
   } else if (topic === "entertainment") {
     apiData.cate = eKey;
+    document.getElementById("topic-image").innerHTML =
+      "<img src='pictures/entertainment-banner.jpg' alt='sportpic' width='90%'/>";
   } else if (topic === "technology") {
     apiData.cate = techKey;
+    document.getElementById("topic-image").innerHTML =
+      "<img src='pictures/tech-banner.jpg' alt='sportpic' width='100%' />";
   }
 
-  newsList.innerHTML = ''
+  newsList.innerHTML = "";
 
-  const {url, lan, cate, apiK} = apiData;
+  const { url, lan, cate, apiK } = apiData;
   const apiUrl = `${proxyUrl}${url}${lan}${cate}${apiK}`;
   console.log(apiUrl);
 
   fetch(apiUrl)
-      .then((result) => result.json())
-      .then((data) => { 
+    .then((result) => result.json())
+    .then((data) => {
       data.articles.forEach((article) => {
         console.log(article);
-        let D = document.createElement('div');
-        let a = document.createElement('a');
-        let i = document.createElement('img');
-        a.setAttribute('href', article.url);
-        a.setAttribute('target', '_blank');
-        a.setAttribute('rel', 'noopener noreferrer');
-        a.setAttribute('style', 'margin-left: 15px;')
+        let D = document.createElement("div");
+        let a = document.createElement("a");
+        let i = document.createElement("img");
+        a.setAttribute("href", article.url);
+        a.setAttribute("target", "_blank");
+        a.setAttribute("rel", "noopener noreferrer");
+        a.setAttribute("style", "margin-left: 15px;");
         a.textContent = article.title;
-        i.setAttribute('style', 'float:left; width: 100px; height: 50px;');
-        i.setAttribute('src', `${article.urlToImage}`);
-        i.setAttribute('alt', "News-image");
-        D.setAttribute('style', 'padding-top: 20px; margin: 50px;');
+        i.setAttribute("style", "float:left; width: 100px; height: 50px;");
+        i.setAttribute("src", `${article.urlToImage}`);
+        i.setAttribute("alt", "News-image");
+        D.setAttribute("style", "padding-top: 20px; margin: 50px;");
         D.appendChild(a);
         D.appendChild(i);
         console.log(D);
         newsList.appendChild(D);
-      })
-    })
+      });
+    });
 }
-
