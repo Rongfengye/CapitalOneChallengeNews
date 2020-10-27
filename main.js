df8eab3ab928ee2db473e432ba755919651cdba7
Rong Feng Ye <rongfengye@Rongs-MBP.fios-router.home>
2020-10-27 15:25:32 -0400

    added news date

--- main.js
+++ main.js
@@ -12,6 +12,21 @@ const apiData = {
   apiK: "apiKey=3680b889e3fe4c128f994a74da6f98db",
 };
 
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const proxyUrl = "http://cors-anywhere.herokuapp.com/";
 
 function buttonHandler(topic) {
@@ -44,19 +59,33 @@ function buttonHandler(topic) {
       data.articles.forEach((article) => {
         console.log(article);
         let D = document.createElement("li");
+        // News Link
         let a = document.createElement("a");
-        let i = document.createElement("img");
         a.setAttribute("href", article.url);
         a.setAttribute("target", "_blank");
         a.setAttribute("rel", "noopener noreferrer");
         a.setAttribute("style", "margin-left: 15px;");
         a.textContent = article.title;
+        // News Image
+        let i = document.createElement("img");
         i.setAttribute("style", "float:left; width: 100px; height: 50px;");
         i.setAttribute("src", `${article.urlToImage}`);
         i.setAttribute("alt", "no image available");
+        // News Date
+        let date = document.createElement("span");
+        date.textContent = `Published: ${
+          months[article.publishedAt.slice(5, 7) - 1]
+        }/${article.publishedAt.slice(
+          8,
+          article.publishedAt.length - 10
+        )}/${article.publishedAt.slice(0, 4)}`;
+        date.className = "news-date";
+        // Finalizing the Div element
         D.className = "news-links";
         D.appendChild(a);
         D.appendChild(i);
+        D.appendChild(document.createElement("br"));
+        D.appendChild(date);
         console.log(D);
         newsList.appendChild(D);
       });
